cea8cb83cf32b38681ee0ea22e3b81b7fd8faeda
zharrane <zakariaharrane2@gmail.com>
2023-11-05 20:02:39 +0100

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import Express from 'express'
 import { logger } from './config/logger'
 import { router } from './routes'
+import cors from 'cors'
 
 const app = Express()
+
+const corsOptions = {
+  origin: '*',
+  // methods:
+  // GET,HEAD,PUT,PATCH,POST,DELETE,
+  // allowedHeaders:
+  //     "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token ,Content-Range" ,
+  exposedHeaders: ['Content-Range'],
+  // optionsSuccessStatus: 204
+}
+
+app.use(cors(corsOptions))
+
+// parse json request body
+app.use(Express.json())
+
+// parse urlencoded request body
+app.use(Express.urlencoded({ extended: true }))
 
 app.use('/v1', router)
 
